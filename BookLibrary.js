142031a552e0b306e21ff34f6854a7cf026cb4d1
JosePascua01 <joelo.pascua@gmail.com>
2024-11-21 15:45:09 -0500

    modified:   BookLibrary.js 	modified:   index.html

--- BookLibrary.js
+++ BookLibrary.js
@@ -13,9 +13,22 @@ function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = () => {
-        return `${this.title} by ${this.author}, ${pages} pages, ${read ? 'already read':'not read yet'}`;
+    this.read = read ? 'is already read.' : 'is not yet read.';
+}
+
+function render(){
+    let libraryBook = document.querySelector("#library");
+    libraryBook.innerHTML = "";
+    for(let i = 0; i < myLibrary.length; i++ ){
+        let book = myLibrary[i];
+        let bookEl = document.createElement('div');
+        bookEl.innerHTML = `
+        <h1>${book.title}</h1> 
+        <p>by ${book.author}</p>
+        <p>has ${book.pages} pages</p>
+        <p>${book.read}</p>
+        `
+        libraryBook.appendChild(bookEl);
     }
 }
 
@@ -23,13 +36,14 @@ function addBookToLibrary(){
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
-    let read = document.querySelector('#read').value;
+    let read = document.querySelector('#read').checked;
     let newBook = new Book(title, author, pages, read);
-    console.log(newBook);
+    myLibrary.push(newBook);
+    render();
 }
 
-newBookForm.addEventListener("submit", ()=>{
-    event.preventDefault();
+newBookForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
     addBookToLibrary();
 })
 
